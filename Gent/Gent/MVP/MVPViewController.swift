50da0072e81525872f87f5889922e9f9ae2f7c78
cmschell <christina.m.schell@gmail.com>
2017-09-25 19:27:42 -0700

    Set up view hierarchy and tab bar

---
+++ Gent/Gent/MVP/MVPViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MVPViewController.swift
+//  Gent
+//
+//  Created by christina schell on 9/25/17.
+//  Copyright © 2017 Christina Sund. All rights reserved.
+//
+
+import UIKit
+
+class MVPViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("MVP Loaded")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
