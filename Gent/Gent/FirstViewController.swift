fc6af43308941316b80150efcbcee1e570a7c258
cmschell <christina.m.schell@gmail.com>
2017-09-25 18:36:22 -0700

    Initial Commit

---
+++ Gent/Gent/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Gent
+//
+//  Created by christina schell on 9/25/17.
+//  Copyright © 2017 Christina Sund. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Gent/GentTests/GentTests.swift
@@ -0,0 +1,36 @@
+//
+//  GentTests.swift
+//  GentTests
+//
+//  Created by christina schell on 9/25/17.
+//  Copyright © 2017 Christina Sund. All rights reserved.
+//
+
+import XCTest
+@testable import Gent
+
+class GentTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
