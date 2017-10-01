c28ffae80ef50b23a21c483487cc5d69b602a092
Christina Sund <ChristinaSund@christinas-MacBook-Pro-2.local>
2017-10-01 14:43:26 -0700

    Commit tab bar updates and table view

--- Gent/Gent/AppDelegate.swift
+++ Gent/Gent/AppDelegate.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    // Initialize Firebase
+    override init() {
+        FirebaseApp.configure()
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
---
+++ Gent/Pods/Target Support Files/Pods-GentUITests/Pods-GentUITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_GentUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GentUITestsVersionString[];
+
