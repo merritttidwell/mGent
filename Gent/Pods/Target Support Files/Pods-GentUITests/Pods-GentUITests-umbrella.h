eb21760366abd53113a918254108d879fdcbb71e
Ossama Mikhail <usama.cpp@gmail.com>
2017-11-24 13:32:17 -0500

    stop tracking pods

--- Gent/Pods/Target Support Files/Pods-GentUITests/Pods-GentUITests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_GentUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_GentUITestsVersionString[];
-
