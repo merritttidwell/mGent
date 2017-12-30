d41005534a912ca17e1737c5c709ab6b8a2117ff
Ossama Mikhail <usama.cpp@gmail.com>
2017-12-29 22:15:10 -0500

    UITextField extension

---
+++ Gent/Gent/New UI/SuperUIs/UITextFieldExt.swift
@@ -0,0 +1,19 @@
+//
+//  UITextFieldExt.swift
+//  Gent
+//
+//  Created by Ossama Mikhail on 12/29/17.
+//  Copyright © 2017 Christina Sund. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField {
+    
+    open override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.borderWidth = 2
+    }
+}
