5f9c0240cfca0a27962552f44de5656f3bbebe4a
Ossama Mikhail <usama.cpp@gmail.com>
2018-01-15 13:42:25 -0500

    firebase functions stripe implementation

--- Gent/Gent/FB_Func/functions/index.js
+++ Gent/Gent/FB_Func/functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+var stripe = require("stripe")("sk_test_Qc3frsXwp1sA8KnQSXC1SU6i");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -31,3 +32,95 @@ exports.trigger1 = functions.database.ref("/users/{uid}").onCreate(event => {
     //return event.data.ref.parent.child('uppercase').set(uppercase);
     return null;
   });
+
+exports.ephemeral_keys = functions.https.onRequest((req, res) => {
+  if (req.method === "POST") {
+    api_version = req.headers["api_version"];
+    cusId = req.headers["customerid"];
+    console.log(api_version);
+    console.log(cusId);
+    if (!api_version || !cusId) {
+      res.status = 500
+      res.end();
+      console.log("issueKeyHandler - error");
+      return;
+    }
+    // This function assumes that some previous middleware has determined the
+    // correct customerId for the session and saved it on the request object.
+    console.log("issueKeyHandler");
+
+    stripe.ephemeralKeys.create(
+      {customer: cusId},
+      {stripe_version: api_version}
+    ).then((ekey) => {
+      console.log(ekey);
+
+      res.status = 200
+      res.end(JSON.stringify(ekey));
+    }).catch((err) => {
+      console.log("error");
+
+      res.status = 500
+      res.end();
+    });
+  }
+});
+
+exports.create_customer = functions.https.onRequest((req,res) => {
+  if (req.method === "POST") {
+    console.log("createCustomer");
+    eid = req.headers["email"]
+    console.log(eid);
+
+    stripe.customers.create({
+      email: eid
+    }, function(err, customer) {
+      //console.log(customer);
+      if (customer != null && err == null) {
+        res.status = 200
+        res.end(JSON.stringify({id: customer.id}));
+      } else {
+        res.status = 500
+        res.end();
+      }
+    });
+  }
+});
+
+exports.charge = functions.https.onRequest((req,res) => {
+  if (req.method === "POST") {
+    console.log("charge");
+    console.log(req.headers);
+
+    cus = req.headers["cus"]
+    card = req.headers["card"]
+    amount = req.headers["amount"]
+    desc = req.headers["desc"]
+    console.log(cus);
+    console.log(card);
+    console.log(amount);
+    console.log(desc);
+
+    stripe.charges.create({
+      amount: amount,
+      currency: "usd",
+      description: desc,
+      customer: cus,
+      card: card
+    }, function(err, charge) {
+      console.log(charge);
+      if (err != null) {
+        console.log("CHARGE ERROR ===>>>");
+        console.log(err);
+      }
+      
+      if (charge != null && err == null) {
+        res.status = 200
+        res.end(JSON.stringify(charge));
+      } else {
+        res.status = 500
+        res.end();
+      }
+    });
+  }
+});
