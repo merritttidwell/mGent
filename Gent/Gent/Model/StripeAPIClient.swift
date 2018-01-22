776b7aa58f12edcde57f93005bd81afc8991ef25
Ossama Mikhail <usama.cpp@gmail.com>
2018-01-21 21:02:58 -0500

    handle calling correct endpoint for dev & prod environments

--- Gent/Gent/Model/StripeAPIClient.swift
+++ Gent/Gent/Model/StripeAPIClient.swift
@@ -13,7 +13,7 @@ import Alamofire
 struct StripeAPIData {
     static let myServer = "https://us-central1-gent-53de7.cloudfunctions.net"   //"http://localhost:3000"
     
-#if DEBUG
+#if PROD
     static let publishableKey = "pk_live_c7Jnj340U7R2mZInbLRIEDb0"//"pk_test_5MG4Sw3JfAGO39ujgbCYkqyD"
 #else
     static let publishableKey = "pk_live_c7Jnj340U7R2mZInbLRIEDb0"
@@ -35,7 +35,11 @@ class StripeAPIClient: NSObject, STPEphemeralKeyProvider {
     private(set) var ephemeralKey = [String: AnyObject]();
     
     func createCustomerKey(withAPIVersion apiVersion: String, completion: @escaping STPJSONResponseCompletionBlock) {
-        let url = self.baseURL.appendingPathComponent("ephemeral_keys")
+        #if PROD
+            let url = self.baseURL.appendingPathComponent("ephemeral_keys_prod")
+        #else
+            let url = self.baseURL.appendingPathComponent("ephemeral_keys_dev")
+        #endif
         Alamofire.request(url, method: .post, headers: [
             "api_version": apiVersion,
             "customerID": cusID
@@ -53,7 +57,11 @@ class StripeAPIClient: NSObject, STPEphemeralKeyProvider {
     }
 
     func createStripeCustomer(email: String, completion: @escaping STPJSONResponseCompletionBlock) {
-        let url = self.baseURL.appendingPathComponent("create_customer")
+        #if PROD
+            let url = self.baseURL.appendingPathComponent("create_customer_prod")
+        #else
+            let url = self.baseURL.appendingPathComponent("create_customer_dev")
+        #endif
         Alamofire.request(url, method: .post, headers: [
             "email": email
             ])
@@ -75,7 +83,11 @@ class StripeAPIClient: NSObject, STPEphemeralKeyProvider {
                         shippingMethod: PKShippingMethod?,
                         completion: @escaping STPJSONResponseCompletionBlock) {
         
-        let url = self.baseURL.appendingPathComponent("charge")
+        #if PROD
+            let url = self.baseURL.appendingPathComponent("charge_prod")
+        #else
+            let url = self.baseURL.appendingPathComponent("charge_dev")
+        #endif
         let params: [String: String] = [
             "card": result.source.stripeID,
             "cus": cusID,
