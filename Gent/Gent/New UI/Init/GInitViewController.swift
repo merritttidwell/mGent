75531e31ac5e08c8518b80f552d455413443379f
Ossama Mikhail <usama.cpp@gmail.com>
2018-02-18 21:55:53 -0500

    update values based on used phone model

--- Gent/Gent/New UI/Init/GInitViewController.swift
+++ Gent/Gent/New UI/Init/GInitViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import SwiftyJSON
 
 class GInitViewController: GUIViewController {
     
@@ -18,15 +20,25 @@ class GInitViewController: GUIViewController {
 
         // Do any additional setup after loading the view.
         
-        signupLabel?.text = "Sign up to MVP for better \(UIDevice.current.modelName) prices"
-        saveLabel?.text = "Save \(11)% on repairs, and get an extra \(22)% when you trade in your phone"
-        
         if GentsUser.firebaseGentsAuth()?.currentUser != nil {
             GentsUser.shared.reloadUserData(completion: { isReloaded in
                 let sb = UIStoryboard.init(name: "Main_NewDesign", bundle: nil)
                 let vc = sb.instantiateViewController(withIdentifier: "tabsController")
                 
                 self.present(vc, animated: false, completion: nil)
+            })
+        } else {
+            GentsConfig.getModelConfig(completed: { specs -> (Void) in
+                DispatchQueue.main.async { [weak self] in
+                    self?.signupLabel?.text = "Sign up to MVP for better \(UIDevice.current.modelName) prices"
+                    
+                    guard specs != nil else {
+                        self?.saveLabel?.text = "Save xx% on repairs, and get an extra xx% when you trade in your phone"
+                        return
+                    }
+                    
+                    self?.saveLabel?.text = "Save \(String(describing: specs!["save"]))% on repairs, and get an extra \(String(describing: specs!["extra"]))% when you trade in your phone"
+                }
             })
         }
     }
